567e1fa4be75e196837a38e017945b13b592a1b4
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-01-18 01:03:20 +0530

    Update simon.js

--- simon.js
+++ simon.js
@@ -8,13 +8,7 @@ let level = 0;
 
 let h2 = document.querySelector("h2");
 
-document.addEventListener("keypress", function(){
-    if(started == false){
-        console.log("game is started");
-        started = true;
-        levelUp();
-    }
-});
+
 
 function gameFlash(btn) {
     btn.classList.add("flash");
